8bd038e7cf93c9e93076bc591ae21bf81bab82ee
Ｗillie <lou@ip-10-0-0-66.ap-northeast-2.compute.internal>
2019-02-28 17:16:26 +0800

    Upload New File

---
+++ ml2019spring-hw1/hw1.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# Program:
+#	User inputs his first name and last name.  Program shows his full name.
+# History:
+# 2015/07/16	VBird	First release
+PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
+export PATH
+
+python test.py $1 $2
+
+exit 0
